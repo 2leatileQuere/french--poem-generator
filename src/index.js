71d14a684a977abaf5af00fe95b0f2f9a55575c1
2leatileQuere <leatilentlhequere@gmail.com>
2024-09-12 18:05:27 +0200

    added basic Javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("hello botswana");
+alert("generating Poem");
+
+function generatePoem(event) {
+  event.preventDefault();
+  alert("Generating Poem");
+
+  new Typewriter("#Poem", {
+    strings: ["What is stronger than the human heart"],
+    autoStart: true,
+    delay:1
+    cursor:"",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
