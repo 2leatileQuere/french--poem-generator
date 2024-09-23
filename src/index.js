26734d5aeb5a45ec43e130f895b57c8ef2a5d161
2leatileQuere <leatilentlhequere@gmail.com>
2024-09-23 15:42:26 +0200

    updates in Js instructions for poem generator

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "b8af296a5456bat0c2aff0ffb9o2bae3";
   let context =
     "You are a genious Poem expert and love to write love short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate a Setswana poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
